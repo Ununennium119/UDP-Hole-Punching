364f9d8acb37f4318ca398ef2373295998a3eece
Mohammad Sadeghi <moh02sadeghi@gmail.com>
2024-01-20 14:18:18 +0330

    Fix stun

--- stun.py
+++ stun.py
@@ -25,8 +25,8 @@ class StunServer:
                 self.id_address_map[source_id] = address
                 if self.ids_map.get(dest_id) == source_id:
                     print(f'Sending ips to ids {source_id} and {dest_id}')
-                    self.socket.sendto(self.id_address_map[dest_id][0].encode(), self.id_address_map[source_id])
-                    self.socket.sendto(self.id_address_map[source_id][0].encode(), self.id_address_map[dest_id])
+                    self.socket.sendto(self.id_address_map[dest_id][0].encode(), (self.id_address_map[source_id][0], 12345))
+                    self.socket.sendto(self.id_address_map[source_id][0].encode(), (self.id_address_map[dest_id][0], 12345))
                     self.ids_map.pop(dest_id)
                 else:
                     print(f'Saving source id: {source_id} and dest id: {dest_id}')
